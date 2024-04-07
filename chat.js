ea64da6360c07770a75b1fe0099325701144ebe1
Sagar Thapa <kingstonboysagar@gmail.com>
2024-04-08 02:40:32 +0545

    chat app project

---
+++ chat.js
@@ -0,0 +1,77 @@
+const sagarSelectorBtn = document.querySelector('#sagar-selector')
+const dipekshyaSelectorBtn = document.querySelector('#dipekshya-selector')
+const chatHeader = document.querySelector('.chat-header')
+const chatMessages = document.querySelector('.chat-messages')
+const chatInputForm = document.querySelector('.chat-input-form')
+const chatInput = document.querySelector('.chat-input')
+const clearChatBtn = document.querySelector('.clear-chat-button')
+
+const messages = JSON.parse(localStorage.getItem('messages')) || []
+
+const createChatMessageElement = (message) => `
+  <div class="message ${message.sender === 'sagar' ? 'blue-bg' : 'gray-bg'}">
+    <div class="message-sender">${message.sender}</div>
+    <div class="message-text">${message.text}</div>
+    <div class="message-timestamp">${message.timestamp}</div>
+  </div>
+`
+
+window.onload = () => {
+  messages.forEach((message) => {
+    chatMessages.innerHTML += createChatMessageElement(message)
+  })
+}
+
+let messageSender = 'sagar'
+
+const updateMessageSender = (name) => {
+  messageSender = name
+  chatHeader.innerText = `${messageSender} chatting...`
+  chatInput.placeholder = `Type here, ${messageSender}...`
+
+  if (name === 'sagar') {
+    sagarSelectorBtn.classList.add('active-person')
+    dipekshyaSelectorBtn.classList.remove('active-person')
+  }
+ if (name === 'dipekshya') {
+    dipekshyaSelectorBtn.classList.add('active-person')
+    sagarSelectorBtn.classList.remove('active-person')
+  } 
+
+  /* auto-focus the input field */
+  chatInput.focus()
+}
+
+sagarSelectorBtn.onclick = () => updateMessageSender('sagar')
+dipekshyaSelectorBtn.onclick = () => updateMessageSender('dipekshya')
+
+const sendMessage = (e) => {
+  e.preventDefault()
+
+  const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  const message = {
+    sender: messageSender,
+    text: chatInput.value,
+    timestamp,
+  }
+
+  /* Save message to local storage */
+  messages.push(message)
+  localStorage.setItem('messages', JSON.stringify(messages))
+
+  /* Add message to DOM */
+  chatMessages.innerHTML += createChatMessageElement(message)
+
+  /* Clear input field */
+  chatInputForm.reset()
+
+  /*  Scroll to bottom of chat messages */
+  chatMessages.scrollTop = chatMessages.scrollHeight
+}
+
+chatInputForm.addEventListener('submit', sendMessage)
+
+clearChatBtn.addEventListener('click', () => {
+  localStorage.clear()
+  chatMessages.innerHTML = ''
+})
